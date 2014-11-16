f3bc3e44e552fdeac9053f44628cc9d5417f2b7a
Wirwing <irving.cf@gmail.com>
2014-11-16 04:02:11 -0600

    Integrando bower

---
+++ admin/app/assets/javascripts/bower.js
@@ -0,0 +1,5 @@
+// Desde Bower
+//= require bootstrap/dist/js/bootstrap
+//= require angular/angular.min
+//= require angular-resource/angular-resource.min
+//= require angular-route/angular-route.min
